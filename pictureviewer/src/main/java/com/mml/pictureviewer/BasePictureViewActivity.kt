c4fb81e253a59cbc9ffc6b340965998caf05b54f
Meng long <mml2015@126.com>
2020-01-03 18:44:33 +0800

    feat:修改

--- pictureviewer/src/main/java/com/mml/pictureviewer/BasePictureViewActivity.kt
+++ pictureviewer/src/main/java/com/mml/pictureviewer/BasePictureViewActivity.kt
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.viewpager.widget.PagerAdapter
+import androidx.viewpager.widget.ViewPager
 import com.github.chrisbanes.photoview.PhotoView
 import kotlinx.android.synthetic.main.activity_base_picture_view.*
 import net.lucode.hackware.magicindicator.ViewPagerHelper
@@ -15,7 +18,7 @@ abstract class BasePictureViewActivity<T> : AppCompatActivity() {
     /**
      * 初始化数据类型
      */
-    abstract fun initData(data:MutableList<T>)
+    abstract fun initData(data:MutableList<T>?)
     /**
      * 使用自己的图片加载框架,用户自己决定采用什么方式显示
      * @param photoView  支持缩放的imageview  [https://github.com/chrisbanes/PhotoView]
@@ -27,25 +30,32 @@ abstract class BasePictureViewActivity<T> : AppCompatActivity() {
         data: T,
         position: Int
     )
-    val mDataList= mutableListOf<T>()
+    val mDataList= MutableLiveData<MutableList<T>>()
+    protected var mViewPager:ViewPager?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base_picture_view)
-        initData(mDataList)
+        mDataList.value= mutableListOf()
+        initData(mDataList.value)
         initViewPager()
         initMagicIndicator()
+        mDataList.observe(this, Observer {
+            mViewPager?.adapter?.notifyDataSetChanged()
+            magic_indicator.navigator.notifyDataSetChanged()
+        })
     }
 
 
     private fun initViewPager(){
-        mViewPager.adapter=object : PagerAdapter() {
+        mViewPager = findViewById(R.id.mViewPager)
+        mViewPager!!.adapter=object : PagerAdapter() {
             override fun isViewFromObject(view: View, any: Any): Boolean =view==any
 
-            override fun getCount(): Int =mDataList.size
+            override fun getCount(): Int =mDataList.value!!.size
             override fun instantiateItem(container: ViewGroup, position: Int): Any {
                 val photoView = PhotoView(container.context)
-                photoView.tag=mDataList[position]
-                onPictureShow(photoView,mDataList[position],position)
+                onPictureShow(photoView,mDataList.value!![position],position)
+                photoView.id = position
                 container.addView(photoView)
                 return photoView
             }
@@ -57,21 +67,21 @@ abstract class BasePictureViewActivity<T> : AppCompatActivity() {
             override fun getItemPosition(any: Any): Int {
                 val photoView =any as PhotoView
 
-                val index: Int = mDataList.indexOf(photoView.tag as T)
+                val index: Int = photoView.id
                 return if (index >= 0) {
                     index
                 } else POSITION_NONE
             }
         }
     }
-    private fun initMagicIndicator(){
+    protected fun initMagicIndicator(){
         val scaleCircleNavigator =
             ScaleCircleNavigator(this)
-        scaleCircleNavigator.setCircleCount(mDataList.size)
+        scaleCircleNavigator.setCircleCount(mDataList.value!!.size)
         scaleCircleNavigator.setNormalCircleColor(Color.LTGRAY)
         scaleCircleNavigator.setSelectedCircleColor(Color.DKGRAY)
         scaleCircleNavigator.setCircleClickListener {
-            mViewPager.currentItem = it
+            mViewPager!!.currentItem = it
         }
         magic_indicator.navigator = scaleCircleNavigator
         ViewPagerHelper.bind(magic_indicator, mViewPager)
